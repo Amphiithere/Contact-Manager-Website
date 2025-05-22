cab07f11ee8ae4729b8d5087598e18c33fc4f498
Jyler <dtj1029@gmail.com>
2025-05-22 18:35:51 -0400

    Fixed register file

--- backend/LAMPAPI/Register.php
+++ backend/LAMPAPI/Register.php
@@ -1,64 +1,72 @@
+
+
 <?php
 
-	$inData = getRequestInfo();
-	
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-        //check if username exists
-		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login = ?");
-        $stmt->bind_param("s", $inData["login"]);
-        $stmt->execute();
-        $stmt->store_result();
+#REGISTER.php
 
-		if ($stmt->num_rows > 0)
-        {
-            returnWithError("Username taken");
-        }
-        else
-        {
-            // Add new user to database
-            $stmt = $conn->prepare("INSERT INTO Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-            $stmt->bind_param("ssss",$inData["firstName"],$inData["lastName"],$inData["login"],$inData["password"]);
-            if ($stmt->execute())
-            {
-                returnWithInfo("User registered successfully");
-            }
-            else
-            {
-                returnWithError("Failed to register user");
-            }
-        }
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+$inData = getRequestInfo();
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $msg )
-	{
-		$retValue = '{"message":"' . $msg . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    $stmt = $conn->prepare("SELECT * FROM Users WHERE Login=?");
+    $stmt->bind_param("s", $inData["login"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        returnWithError("User already exists with this login");
+        $stmt->close();
+        $conn->close();
+        return;
+    }
+
+    $hashedPassword = password_hash($inData["password"], PASSWORD_DEFAULT);
+    $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $hashedPassword);
+    $stmt->execute();
+
+    if($stmt->affected_rows > 0)
+    {
+        $userId = $stmt->insert_id;
+    }
+    else
+    {
+        returnWithError("Error creating user");
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+function returnWithInfo( $message )
+{
+    $retValue = '{"success":true,"message":"' . $message . '"}';
+    sendResultInfoAsJson( $retValue );
+}
 ?>
